fa2b8858e677b525a2c51a9d3bb3b5d43b5f461b
aymanFayez <aymanfayez277@gmail.com>
2022-07-09 07:48:04 +0200

    Adding ZoomIn, ZoomOut feature

--- CandelChart/CandelChart/ViewController.swift
+++ CandelChart/CandelChart/ViewController.swift
@@ -14,9 +14,14 @@ class ViewController: UIViewController, ChartViewDelegate {
     private var backgroundColor = UIColor(red: 48/255, green: 48/255, blue: 48/255, alpha: 1)
     private var blueColor = UIColor(red: 0, green: 203/255, blue: 252/255, alpha: 1)
     private var redColor = UIColor(red: 1, green: 81/255, blue: 85/255, alpha: 1)
+    private var data = CandleChartData()
+    private var sliderXValue: Float = 15
+    private var sliderYValue: Float = 50
     
     // MARK: - Outlets
     @IBOutlet weak var candleChartView: CandleStickChartView!
+    @IBOutlet weak var zoomInButton: UIButton!
+    @IBOutlet weak var zoomOutButton: UIButton!
     
     // MARK: - Methods
     override func viewDidLoad() {
@@ -37,6 +42,8 @@ class ViewController: UIViewController, ChartViewDelegate {
         candleChartView.pinchZoomEnabled = true
         candleChartView.legend.enabled = false
         candleChartView.xAxis.labelPosition = .bottom
+        zoomInButton.layer.cornerRadius = 2
+        zoomOutButton.layer.cornerRadius = 2
     }
     
     private func setupRightAxis() {
@@ -57,55 +64,45 @@ class ViewController: UIViewController, ChartViewDelegate {
         candleChartView.xAxis.labelTextColor = .gray
     }
     
-    func updateChartData() {
-        self.setDataCount(Int(10), range: UInt32(50), isIncreasing: false)
+    private func updateChartData() {
+        // Increasing entries of currencies
+        let entry1 = CandleChartDataEntry(x: Double(5), shadowH: 60, shadowL: 10, open: 20, close: 50, icon: UIImage(named: "down-arrow"))
+        let entry2 = CandleChartDataEntry(x: Double(10), shadowH: 70, shadowL: 20, open: 30, close: 60, icon: UIImage(named: "down-arrow"))
+        
+        // Decreasing entries of currencies
+        let entry3 = CandleChartDataEntry(x: Double(15), shadowH: 10, shadowL: 60, open: 50, close: 20, icon: UIImage(named: "up-arrow"))
+        let entry4 = CandleChartDataEntry(x: Double(20), shadowH: 20, shadowL: 70, open: 60, close: 30, icon: UIImage(named: "up-arrow"))
+        
+        addCurrencyEntry([entry1, entry2])
+        addCurrencyEntry([entry3, entry4])
     }
     
-    func setDataCount(_ count: Int, range: UInt32, isIncreasing: Bool) {
-        let yVals1 = (0..<count).map { (i) -> CandleChartDataEntry in
-                let mult = range + 1
-                let val = Double(arc4random_uniform(40) + mult)
-                let high = Double(arc4random_uniform(9) + 8)
-                let low = Double(arc4random_uniform(9) + 8)
-                let open = Double(arc4random_uniform(6) + 1)
-                let close = Double(arc4random_uniform(6) + 1)
-                let even = i % 2 == 0
-                
-                return CandleChartDataEntry(x: Double(i), shadowH: val + high, shadowL: val - low, open: even ? val + open : val - open, close: even ? val - close : val + close, icon: UIImage(named: "icon"))
-        }
-        
-        let set1 = CandleChartDataSet(entries: yVals1, label: "Data Set")
-        set1.axisDependency = .right
-        set1.setColor(UIColor(white: 80/255, alpha: 1))
-        set1.drawIconsEnabled = false
-        set1.shadowColor = .darkGray
-        set1.shadowWidth = 0.7
-        set1.decreasingColor = redColor
-        set1.decreasingFilled = true
-        set1.increasingColor = blueColor
-        set1.increasingFilled = true
-        set1.neutralColor = .blue
-        let data = CandleChartData(dataSet: set1)
-        
-        let yVals2 = (0..<count).map { (i) -> CandleChartDataEntry in
-            return CandleChartDataEntry(x: Double(i + 10), shadowH: 60, shadowL: 10, open: 20, close: 50, icon: UIImage(named: "icon"))
-        }
-        let set2 = CandleChartDataSet(entries: yVals2, label: "Data Set")
-        set2.axisDependency = .right
-        set2.setColor(UIColor(white: 80/255, alpha: 1))
-        set2.drawIconsEnabled = false
-        set2.shadowColor = .darkGray
-        set2.shadowWidth = 0.7
-        set2.decreasingColor = redColor
-        set2.decreasingFilled = true
-        set2.increasingColor = blueColor
-        set2.increasingFilled = true
-        set2.neutralColor = .blue
-        data.append(set2)
-        
+    private func addCurrencyEntry(_ entries: [CandleChartDataEntry]) {
+        let set = CandleChartDataSet(entries: entries, label: "")
+        set.axisDependency = .right
+        set.setColor(UIColor(white: 80/255, alpha: 1))
+        set.drawIconsEnabled = false
+        set.shadowColor = .darkGray
+        set.shadowWidth = 0.7
+        set.decreasingColor = redColor
+        set.decreasingFilled = true
+        set.increasingColor = blueColor
+        set.increasingFilled = true
+        set.neutralColor = .blue
+        set.valueTextColor = .clear
+        set.drawIconsEnabled = true 
+        data.append(set)
         candleChartView.data = data
     }
 
-
+    @IBAction func didPressZoomIn(_ sender: Any) {
+        candleChartView.zoomIn()
+    }
+    
+    @IBAction func didPressZoomOut(_ sender: Any) {
+        candleChartView.zoomOut()
+    }
+    
+    
 }
 
